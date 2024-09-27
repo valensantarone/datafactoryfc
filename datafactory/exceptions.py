82926137a0657b47a8968812dac573a41a96916c
valentino <valensantarone@gmail.com>
2024-09-27 09:49:27 -0300

    Adding InvalidMatchInput exception

--- datafactory/exceptions.py
+++ datafactory/exceptions.py
@@ -2,3 +2,8 @@ class MatchDoesntHaveInfo(Exception):
     def __init__(self):
         self.message = f"Match doesn't have the required information."
         super().__init__(self.message)
+        
+class InvalidMatchInput(Exception):
+    def __init__(self):
+        self.message = f"The input given is not valid. Must be the json data of the match or an array with league name (str) and match id (int), in that order."
+        super().__init__(self.message)
