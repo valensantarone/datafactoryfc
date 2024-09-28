5c0bc06f8b4eebb6de5a38271225a724beda28ca
valko <valensantarone@gmail.com>
2024-09-28 16:22:19 -0300

    Change name

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from setuptools import setup, find_packages
 
 setup(
-    name="datafactory",
+    name="data-factory",
     version="1.0.0",
     author="Valko",
     author_email="valensantarone@gmail.com",
